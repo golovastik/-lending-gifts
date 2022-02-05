41daa306e2ba7a51fb57f0cd103d923038ffd004
golovastik <golovastik@exam.com>
2022-02-05 21:36:25 +0300

    added more files 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-var gulp = require('gulp')
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    browserSync = require('browser-sync')
 
 
 gulp.task('html', function () {
     return gulp.src('*.html')
         .pipe(gulp.dest('build'))
 })
+gulp.task('sass', function(){
+    return gulp.src('sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'))
+})
 
 
-gulp.task("build", gulp.series('html'))
+gulp.task("build", gulp.series('html', 'sass', function(){
+    browserSync.init({
+        server: "build"
+    });
+    gulp.watch("scss/**/*.scss", gulp.parallel("sass"))
+    gulp.watch("*.html", gulp.parallel('html'))
+}))
