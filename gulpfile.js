91f4f9a0803060e5b6bd9d061c1ebb3916edf537
golovastik <golovastik@exam.com>
2022-02-06 20:08:32 +0300

    added more file 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,54 @@
+
 var gulp = require('gulp'),
     sass = require('gulp-sass')(require('sass')),
-    browserSync = require('browser-sync')
+    browserSync = require('browser-sync'),
+    imagemin = require('gulp-imagemin'),
+    imageminJpegRecompress = require('imagemin-jpeg-recompress')
 
 
 gulp.task('html', function () {
     return gulp.src('*.html')
         .pipe(gulp.dest('build'))
 })
-gulp.task('sass', function(){
+gulp.task('sass', function () {
     return gulp.src('sass/style.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('build/css'))
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'))
+})
+gulp.task('allimg', function () {
+    return gulp.src('img/*.{png, svg, jpg}')
+        .pipe(gulp.dest('build/img'))
+})
+
+gulp.task('images', function () {
+    return gulp.src('img/*.{png, svg, jpg}')
+        .pipe(imagemin([
+            // gulp.imagemin.jpegtran({ progressive: true }),
+            // gulp.imageminJpegRecompress({
+            //     loops: 5,
+            //     min: 65,
+            //     max: 70,
+            //     quality: 'medium'
+            // }),
+            // gulp.imagemin.optipng({ optimizationLevel: 3 }),
+            // gulp.pngquant({ quality: [0.6, 0.7], speed: 5 }),
+            // gulp.imagemin.svgo()
+        ]))
+        .pipe(gulp.dest('build/img'));
 })
 
 
-gulp.task("build", gulp.series('html', 'sass', function(){
+
+
+gulp.task('serve', function () {
     browserSync.init({
         server: "build"
     });
     gulp.watch("scss/**/*.scss", gulp.parallel("sass"))
     gulp.watch("*.html", gulp.parallel('html'))
-}))
+})
+
+
+
+gulp.task('build', gulp.series('html', 'sass', 'images'))
+
